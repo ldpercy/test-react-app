6918f01b439238a7259696040d4abd0a7df067ef
Paul Dale <ldpercy@gmail.com>
2021-05-11 12:58:49 +1000

    convert whitespace

--- src/main/body.js
+++ src/main/body.js
@@ -7,32 +7,28 @@ import Bar from '../module/bar';
 
 class Body extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { };
-    }
+	constructor(props) {
+		super(props);
+		this.state = { };
+	}
 
 
-    render() {
-        return (
-            <div>
+	render() {
+		return (
+			<div>
+				<p>Test ReactsJs app</p>
+				<hr/>
 
-                <p>Test ReactsJs app</p>
-                <hr/>
+				<Foo number="123"/>
+				<Foo number="456"/>
 
-                <Foo number="123"/>
-                <Foo number="456"/>
+				<hr/>
 
-                <hr/>
+				<Bar list={['one','two','three','four','five']}/>
 
-                <Bar list={['one','two','three','four','five']}/>
-
-
-                <hr/>
-
-            </div>
-        );
-    }
+			</div>
+		);
+	}
 
 }
 
